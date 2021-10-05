14b7043444cdf61e73a5bd6684f3b97444a680aa
mnawa3 <fouratir16@gmail.com>
2021-10-05 15:15:42 +0100

    created groups and lectures schemas

--- database/models/groupsModel.js
+++ database/models/groupsModel.js
@@ -1,8 +1,18 @@
+
 const mongoose = require("mongoose");
 const client = require("../database");
 mongoose.Promise = global.Promise;
 
-const groupsSchema = mongoose.Schema({});
+const groupsSchema = mongoose.Schema({
+    name:{type:String, unique:true},
+    membersNumber:Number,
+    lecturesNumber:Number,
+    adminId:String,
+    membersId:[String],
+    lecturesId:[String],
+    requestsId:[String],
+
+});
 
 const Groups = mongoose.model("Groups", groupsSchema);
 
