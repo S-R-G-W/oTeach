55a0ccf5e688cdcaef506225d55aa4c0dcaad7ac
mnawa3 <fouratir16@gmail.com>
2021-10-05 15:30:23 +0100

    try

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const db = require("../database/database");
+const cors = require("cors")
 
 const port = 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
 
 app.listen(port, () => {
