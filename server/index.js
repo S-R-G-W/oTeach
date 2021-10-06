3e2e39d76f39df25693ef396f972598d451d77b8
WissemBrinis <wissembenbrinis@gmail.com>
2021-10-06 13:15:06 +0100

    still trying

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const db = require("../database/database");
-
+const cors = require("cors");
 
 const port = 8000;
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
