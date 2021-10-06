6cb0c85fc1bfcdbd9305ef165c4ca3196ac2d727
WissemBrinis <wissembenbrinis@gmail.com>
2021-10-06 12:51:32 +0100

    app changes

--- server/index.js
+++ server/index.js
@@ -2,12 +2,11 @@ const express = require("express");
 const app = express();
 const db = require("../database/database");
 
+
 const port = 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname, "../public"));
-
 
 app.listen(port, () => {
   console.log(`listening on port  ${port}`);
--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React, { Component } from "react";
 import "./App.css";
 import SignUp from "./components/signup.js";
 import axios from "axios";
+import Home from "./components/Home";
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
+      view: "home",
       name: "",
       email: "",
       password: "",
     };
     this.handleChange = this.handleChange.bind(this);
     this.signupToDataBase = this.signupToDataBase.bind(this);
+    this.changeView = this.changeView.bind(this);
+    this.renderView = this.renderView.bind(this);
   }
 
   handleChange(e) {
@@ -25,13 +29,51 @@ export default class App extends Component {
     });
   }
 
+  changeView(option) {
+    this.setState({
+      view: option,
+    });
+  }
+
+  renderView() {
+    const view = this.state.view;
+    if (view === "home") {
+      return <Home test={this.changeView} />;
+    } else if (view === "signup") {
+      return <SignUp />;
+    }
+  }
+
   render() {
     return (
       <div>
-        <SignUp
-          handleChange={this.handleChange}
-          signupToDataBase={this.signupToDataBase}
-        />
+        <div className="nav">
+          <span className="logo" onClick={() => this.changeView("home")}>
+            oTeach
+          </span>
+          <span
+            className={
+              this.state.view === "home" ? "nav-selected" : "nav-unselected"
+            }
+            onClick={() => this.changeView("home")}
+          ></span>
+          <span
+            className="nav-unselected"
+            onClick={() => this.changeView("signup")}
+          >
+            SignUp
+          </span>
+          <span
+      
+            onClick={() => {
+              this.changeView("login");
+            }}
+          >
+            Login
+          </span>
+        </div>
+
+        <div className="main">{this.renderView()}</div>
       </div>
     );
   }
