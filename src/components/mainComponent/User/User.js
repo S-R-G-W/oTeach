1043df3903f80dad69a59490685fbf27d3ce3c13
ghassen-ben-mechlia <mechliaghassen@gmail.com>
2021-10-10 18:02:18 +0100

    pull request

--- src/components/mainComponent/User/User.js
+++ src/components/mainComponent/User/User.js
@@ -18,40 +18,146 @@ export default class User extends Component {
 
   render() {
     return (
-      <section>
-      <div id="lgx-services" class="lgx-services lgx-services-black"> 
-          <div class="lgx-inner">
-              <div class="container">
-                  <div class="row">
-                      <div class="col-sm-6 col-md-4">
-                          <div class="lgx-single-service lgx-single-service-white"> 
-                              <span class="icon"><i class="fa fa-play-circle-o" aria-hidden="true"></i></span>
-                              <h2 class="title"><Link to="/CreateGroup">Create Group</Link></h2>
-                              <p>Etiam vel ante ac lacus vestibulum rutrum. Aliquam vehicula, massa in auctor dapibus commodo quis vehicula lacus metus sed justo. </p>
-                              <a class="lgx-btn-simple" href="#">Read More</a>
-                          </div>
-                      </div>
-                      <div class="col-sm-6 col-md-4">
-                          <div class="lgx-single-service lgx-single-service-white"> 
-                              <span class="icon"><i class="fa fa-address-book" aria-hidden="true"></i></span>
-                              <h2 class="title"><a href="#">Join Group</a></h2>
-                              <p>Etiam vel ante ac lacus vestibulum rutrum. Aliquam vehicula, massa in auctor dapibus commodo quis vehicula lacus metus sed justo. </p>
-                              <a class="lgx-btn-simple" href="#">Read More</a>
-                          </div>
-                      </div>
-                      <div class="col-sm-6 col-md-4">
-                          <div class="lgx-single-service lgx-single-service-white"> 
-                              <span class="icon"><i class="fa fa-address-book" aria-hidden="true"></i></span>
-                              <UserGroups JoinedGroups={this.props.JoinedGroups}  Createdgroups={this.props.Createdgroups}  handleGroup={this.props.handleGroup} user={this.props.user} /> 
-                          </div>
-                      </div>
-                     
-                  </div>
-              </div>
-          </div>
-      </div>
-  </section>
-  )}
+        <section>
+        <div id="lgx-blog" class="lgx-blog lgx-blog-normal">
+            <div class="lgx-inner">
+                <div class="container">
+                    <div class="row">
+    
+                       
+                        <div class="lgx-news-single">
+                           
+                            <div class="text-area">
+                                <h3 class="title"><a href="news-single.html">The 10 best educational websites apps to learn new stuff for free Education Issues</a></h3>
+                                <p class="text">Lorem ipsum dolor sit amet, consectetuer adir elit. Aenean commodo ligula eget ...</p>
+                                <div class="hits-area">
+                                    <span class="date"></span>
+                                </div>
+                                <div class="text-bottom">
+                                    <a class="date" href="#">25 July 2016</a>
+                                    <a class="link" href="news-single.html"><i class="fa  fa-long-arrow-right" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="lgx-news-single">
+                            
+                            <div class="text-area">
+                                <h3 class="title"><a href="news-single.html">UX Education: Designing free Online Learning Curriculum</a></h3>
+                                <p class="text">Lorem ipsum dolor sit amet, consectetuer adir elit. Aenean commodo ligula eget ...</p>
+                                <div class="hits-area">
+                                    <span class="date"></span>
+                                </div>
+                                <div class="text-bottom">
+                                    <a class="date" href="#">25 July 2016</a>
+                                    <a class="link" href="news-single.html"><i class="fa  fa-long-arrow-right" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div> 
+                        <div class="lgx-news-single">
+                         
+                            <div class="text-area">
+                                <h3 class="title"><a href="news-single.html">The digital revolution in higher education has already happened</a></h3>
+                                <p class="text">Lorem ipsum dolor sit amet, consectetuer adir elit. Aenean commodo ligula eget ...</p>
+                                <div class="hits-area">
+                                    <span class="date"></span>
+                                </div>
+                                <div class="text-bottom">
+                                    <a class="date" href="#">25 July 2016</a>
+                                    <a class="link" href="news-single.html"><i class="fa  fa-long-arrow-right" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div> 
+                        <div class="lgx-news-single">
+                            
+                            <div class="text-area">
+                                <h3 class="title"><a href="news-single.html">I no longer understand my PhD dissertation on Education</a></h3>
+                                <p class="text">Lorem ipsum dolor amet, consectetuer adir elit. Aenean commodo ligula dolor ...</p>
+                                <div class="hits-area">
+                                    <span class="date"></span>
+                                </div>
+                                <div class="text-bottom">
+                                    <a class="date" href="#">25 July 2016</a>
+                                    <a class="link" href="news-single.html"><i class="fa  fa-long-arrow-right" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div> 
+                        <div class="lgx-news-single">
+                           
+                            <div class="text-area">
+                                <h3 class="title"><a href="news-single.html">I no longer understand my PhD dissertation on Education</a></h3>
+                                <p class="text">Lorem ipsum dolor amet, consectetuer adir elit. Aenean commodo ligula dolor ...</p>
+                                <div class="hits-area">
+                                    <span class="date"></span>
+                                </div>
+                                <div class="text-bottom">
+                                    <a class="date" href="#">25 July 2016</a>
+                                    <a class="link" href="news-single.html"><i class="fa  fa-long-arrow-right" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div> 
+                        <div class="lgx-news-single">
+                           
+                            <div class="text-area">
+                                <h3 class="title"><a href="news-single.html">I no longer understand my PhD dissertation on Education</a></h3>
+                                <p class="text">Lorem ipsum dolor amet, consectetuer adir elit. Aenean commodo ligula dolor ...</p>
+                                <div class="hits-area">
+                                    <span class="date"></span>
+                                </div>
+                                <div class="text-bottom">
+                                    <a class="date" href="#">25 July 2016</a>
+                                    <a class="link" href="news-single.html"><i class="fa  fa-long-arrow-right" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div> 
+                        <div class="lgx-news-single">
+                            
+                            <div class="text-area">
+                                <h3 class="title"><a href="news-single.html">UX Education: Designing free Online Learning Curriculum</a></h3>
+                                <p class="text">Lorem ipsum dolor sit amet, consectetuer adir elit. Aenean commodo ligula eget ...</p>
+                                <div class="hits-area">
+                                    <span class="date"></span>
+                                </div>
+                                <div class="text-bottom">
+                                    <a class="date" href="#">25 July 2016</a>
+                                    <a class="link" href="news-single.html"><i class="fa  fa-long-arrow-right" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div> 
+                        <div class="lgx-news-single">
+                            
+                            <div class="text-area">
+                                <h3 class="title"><a href="news-single.html">The digital revolution in higher education has already happened</a></h3>
+                                <p class="text">Lorem ipsum dolor sit amet, consectetuer adir elit. Aenean commodo ligula eget ...</p>
+                                <div class="hits-area">
+                                    <span class="date"></span>
+                                </div>
+                                <div class="text-bottom">
+                                    <a class="date" href="#">25 July 2016</a>
+                                    <a class="link" href="news-single.html"><i class="fa  fa-long-arrow-right" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div> 
+                        <div class="lgx-news-single">
+                       
+                            <div class="text-area">
+                                <h3 class="title"><a href="news-single.html">I no longer understand my PhD dissertation on Education</a></h3>
+                                <p class="text">Lorem ipsum dolor amet, consectetuer adir elit. Aenean commodo ligula dolor ...</p>
+                                <div class="hits-area">
+                                    <span class="date"></span>
+                                </div>
+                                <div class="text-bottom">
+                                    <a class="date" href="#">25 July 2016</a>
+                                    <a class="link" href="news-single.html"><i class="fa  fa-long-arrow-right" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div> 
+    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+ )}
 }
 
 
